ea24e7e1ec896b936722e34367617df208f48a35
Agustín Carbone <agustincarbone22@gmail.com>
2026-05-27 21:50:50 -0300

    Forzando Python 3.11 estable y psycopg2

--- app.py
+++ app.py
@@ -17,10 +17,9 @@ app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY', 'pedro-joyas-secret-2026')
 
 # Database
-# --- CONFIGURACIÓN DE BASE DE DATOS ---
 db_url = os.environ.get('DATABASE_URL', 'sqlite:///jewelry_store.db')
 if db_url.startswith("postgresql://"):
-    db_url = db_url.replace("postgresql://", "postgresql+pg8000://", 1)
+    db_url = db_url.replace("postgresql://", "postgresql+psycopg2://", 1)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {"pool_pre_ping": True}
